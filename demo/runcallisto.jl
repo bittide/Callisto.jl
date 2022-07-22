913ef41848b7239883e903c4ca238b1005d2f553
Sanjay Lall <sanjaylall@google.com>
2022-07-22 11:40:57 -0700

    copied demo script from googlesource

---
+++ demo/runcallisto.jl
@@ -0,0 +1,39 @@
+
+module Run
+
+##############################################################################
+using Callisto
+##############################################################################
+
+using PlotKit
+const pk = PlotKit
+
+plotpath(x) = joinpath(ENV["HOME"], "plots/", x)
+callistox(c) = parse_callisto_logx(c, callisto(c)...)
+
+
+pzip(a,b) = Point.(zip(a,b))
+pzip(a) = pzip(a[1],a[2])
+
+plot(p, f; kwargs...) = save(drawplot(pzip(xy(p)) ; kwargs...), f)
+plot(p::Array, f; kwargs...) = save(drawplot(pzip.(xy.(p)); kwargs...), f)
+save(d, fn) =  pk.save(d, plotpath(fn), 4)
+
+
+
+# simplest run
+function main()
+    c = CalOpts(; kp = 2e-8)
+    xc = callistox(c)
+    plot(xc.freq,  "freqs.pdf")
+    plot(xc.theta, "phase.png")
+    plot(xc.mocc,  "mocc.pdf"; xmax=500e6)
+
+    fc = focused_callisto_info(c, xc, 125e6, 125e6 + 1e2)
+    plot(fc.occ, "occupancy.pdf")
+
+    return xc
+end
+
+
+end
